0c877aba525d3d34aa55d1d0f5f73fba47477602
korgaonkarparth2522-max <korgaonkarparth2522@gmail.com>
2026-04-05 11:12:04 +0530

    day-4-complete

---
+++ Day-4/problem1.py
@@ -0,0 +1,14 @@
+# input = {"name": "Alice", "age": 30}
+# if "age" in input:
+#     print("Age key exists")
+# else:
+#     print("Age key does not exist")
+
+# input = [1,2,2,3,4,3,5]
+# new = {}
+# for i in input:
+#     if i in new:
+#         new[i] += 1
+#     else:
+#         new[i] = 1
+# print(new)
